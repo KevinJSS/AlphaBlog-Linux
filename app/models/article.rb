45825111e173c466f918eaedcde9d02fe31dfe8f
KevinJSS <103754829+KevinJSS@users.noreply.github.com>
2022-08-05 16:57:30 -0600

    Article Details (front-end) feature added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+    #Active Record Validations
+    validates :title, presence: {message: 'is required'}, length: { minimum: 6, maximum: 100 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
