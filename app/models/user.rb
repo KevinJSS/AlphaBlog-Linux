3f6f1f148270578ef86ced52836a04db42e627ee
KevinJSS <103754829+KevinJSS@users.noreply.github.com>
2022-09-13 11:00:10 -0600

    Add user validations for username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false },
+                       length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  # This is a regular expression that matches the RFC 5322 standard for email addresses.
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # format: { with: VALID_EMAIL_REGEX }
 end
