071b8fcfe6bc235016214d905732bafbc9bc17ec
kevin.alvarado@ucrso.info <103754829+KevinJSS@users.noreply.github.com>
2022-09-14 11:26:33 -0600

    Add user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to article_url(@articles_path), notice: "Welcome to the Alpha Blog #{@user.username}, you have signed up successfully." }
+        format.json { render :show, status: :created, location: @articles }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
