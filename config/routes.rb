eedda46bdb6fd251ded3a6fe1be6794b24aa11e9
kevin.alvarado@ucrso.info <103754829+KevinJSS@users.noreply.github.com>
2022-09-14 10:55:01 -0600

    Add user sign routes and form (view)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root 'pages#home'
-  get 'about', to: 'pages#about'
+  root "pages#home"
+  get "about", to: "pages#about"
   resources :articles
+
+  get "signup", to: "users#new" # This is the route for the signup page.
+  # post "users", to: "users#create" # This is the route for the form submission.
+  resources :users, except: [:new] # This is the same as: resources :users, except: [:new, :create]
 end
