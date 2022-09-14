0cd36c9ede66d7d21c82a7fff9a7c8f2a094300f
kevin.alvarado@ucrso.info <103754829+KevinJSS@users.noreply.github.com>
2022-09-14 10:15:25 -0600

    User validations before saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
+  has_many :articles #, dependent: :destroy
+  before_save { self.email = email.downcase }
+
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
                        length: { minimum: 3, maximum: 25 }
+
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false },
                     length: { maximum: 105 },
                     format: { with: URI::MailTo::EMAIL_REGEXP }
-
-  has_many :articles #, dependent: :destroy
 
   # This is a regular expression that matches the RFC 5322 standard for email addresses.
   # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
