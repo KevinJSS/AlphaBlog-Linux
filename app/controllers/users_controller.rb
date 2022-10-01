475089b80637852c35bb056ad1f5b7af4d366f25
kevin.alvarado@ucrso.info <103754829+KevinJSS@users.noreply.github.com>
2022-09-30 18:36:59 -0600

    Add pagination to blogger's articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
   def new
