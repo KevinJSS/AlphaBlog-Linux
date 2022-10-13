eaf9bb9b347c28ba7a5e271c7c85a444b0b4a8b1
kevin.alvarado@ucrso.info <103754829+KevinJSS@users.noreply.github.com>
2022-10-13 11:08:15 -0600

    Add delete user functionallity (with dependent destroy)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    
+
     respond_to do |format|
       if @user.save
         session[:user_id] = @user.id
@@ -42,6 +42,16 @@ class UsersController < ApplicationController
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+
+    respond_to do |format|
+      format.html { redirect_to articles_path, notice: "Account and all associated articles successfully deleted" }
+      format.json { head :no_content }
     end
   end
 
